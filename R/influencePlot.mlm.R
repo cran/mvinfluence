4cc1c540d33f2d7b28611b03ed33f3010c914ccc
Michael Friendly <friendly@yorku.ca>
2016-06-08 17:14:53 +0000

    version 0.8

--- R/influencePlot.mlm.R
+++ R/influencePlot.mlm.R
@@ -1,7 +1,7 @@
 influencePlot.mlm <-
-function(model, scale=12, type=c("cookd", "stres", "LR"),
+function(model, scale=12, type=c("stres", "LR", "cookd"),
 		infl=mlm.influence(model, do.coef = FALSE), FUN=det,
-    fill=TRUE, fill.col="blue",
+    fill=TRUE, fill.col="red",
     fill.alpha.max=0.5,
     labels, 
     id.method = "noteworthy",
@@ -21,9 +21,11 @@ function(model, scale=12, type=c("cookd", "stres", "LR"),
 	if(missing(labels)) labels <- rownames(df)
 	
 	p <- nrow(coef(model))
+	q <- ncol(coef(model))
 	n <- length(infl$H)
 	scale=scale/max(sqrt(CookD))
-	if (fill) cols <- heplots:::trans.colors(fill.col, alpha=fill.alpha.max * CookD/(max(CookD)))
+	# TODO: replace with grDevices::adjustcolor to avoid CRAN warning
+	if (fill) cols <- heplots::trans.colors(fill.col, alpha=fill.alpha.max * CookD/(max(CookD)))
 
 	if(id.method != "identify"){
 	  which.rstud <- order(R, decreasing=TRUE)[1:id.n]
@@ -39,14 +41,15 @@ function(model, scale=12, type=c("cookd", "stres", "LR"),
 #		abline(h=qf(.95, 1, n-p), lty=ref.lty, col=ref.col)
 		abline(h=4/(n-p), lty=ref.lty, col=ref.col)
 		abline(v=c(2, 3)*p/n, lty=ref.lty, col=ref.col)
-		noteworthy <- car:::showLabels(H, CookD, labels=labels, id.method=id.method, 
+		noteworthy <- car::showLabels(H, CookD, labels=labels, id.method=id.method, 
     	id.n=id.n, id.cex=id.cex, id.col=id.col)
 	}
 	else if (type=='stres') {
-		plot(H, Q, xlab="Hat value", ylab="Squared Residual", cex=scale*CookD, ...)
+		plot(H, Q, xlab="Hat value", ylab="Squared Studentized Residual", cex=scale*CookD, ...)
 		if (fill) points(H, Q, cex=scale*CookD, pch=16, col=cols)
 		abline(v=c(2, 3)*p/n, lty=ref.lty, col=ref.col)
-		noteworthy <- car:::showLabels(H, Q, labels=labels, id.method=id.method, 
+		abline(h=qbeta(.95, q/2, (n-p-q)/2), lty=ref.lty, col=ref.col)
+		noteworthy <- car::showLabels(H, Q, labels=labels, id.method=id.method, 
     	id.n=id.n, id.cex=id.cex, id.col=id.col)
 	}
 	else if (type=='LR') {
@@ -58,7 +61,7 @@ function(model, scale=12, type=c("cookd", "stres", "LR"),
 		xmax <- ceiling(par("usr")[2])
 		# FIXME: bit of a kludge in calculating intercepts of diagonal lines
 		for(a in (2*xmin):xmax) abline(a=a, b=-1, col=ref.col, lty=ref.lty)	
-		noteworthy <- car:::showLabels(logL, logR, labels=labels, id.method=id.method, 
+		noteworthy <- car::showLabels(logL, logR, labels=labels, id.method=id.method, 
     	id.n=id.n, id.cex=id.cex, id.col=id.col)
 		}
 #browser()
@@ -67,6 +70,7 @@ function(model, scale=12, type=c("cookd", "stres", "LR"),
 	  	## FIXME:  m>1 car::showLabels returns the labels, not the indices
 	  	if (m>1) noteworthy <- inf
 	  	res <- data.frame(H, Q, CookD, L, R)[noteworthy,]
+	  	rownames(res) <- labels[noteworthy]
 			return(res)
 		}
 
